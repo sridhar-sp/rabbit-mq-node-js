2f3b2800e60436e24ff1badd95414ae500a03dfa
sridhar.subramani <>
2020-11-30 20:48:11 +0530

    Add express

--- build/app.js
+++
@@ -1,2 +0,0 @@
-"use strict";
-console.log('Application is running');
--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,12 @@
-console.log('Application is running');
+import express from "express";
+
+const app = express();
+const PORT = 3000;
+
+app.get("/", (req: express.Request, res: express.Response) => {
+  res.send("Welcome ");
+});
+
+app.listen(PORT, () => {
+  console.log(`Application is running at ${PORT}`);
+});
